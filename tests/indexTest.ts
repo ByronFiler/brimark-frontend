472eb36e432c2ec24f0c83bd20025e75eacc6a6b
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2021-02-18 14:36:28 +0000

    updated the tests to be split into two parts, one the creation of the HTMLPararaphElement and two, that the textcontent has changed.

--- tests/indexTest.ts
+++ tests/indexTest.ts
@@ -1,11 +1,16 @@
 import { createParagraphWithMessage } from '../src/index';
 import { expect } from 'chai';
 
-describe('createStartMessage function', () => {
-    it('should return new HTMLParagraphElement',  () => {
-        const message : string = "Hello, welcome to parcel.js!";
-        const startMessage : HTMLParagraphElement = createParagraphWithMessage(message);
-        expect(startMessage instanceof HTMLParagraphElement).to.equal(true);
-        expect(startMessage.textContent).to.equal(message);
+describe('index Tests', () => {
+    describe('createParagraphWithMessage', () => {
+        it('should return a new HTMLParagraphElement',  () => {
+            const startMessage : HTMLParagraphElement = createParagraphWithMessage("test");
+            expect(startMessage instanceof HTMLParagraphElement).to.equal(true);
+        });
+        it('should set text to message', () => {
+            const message : string = "Hello, welcome to parcel.js!";
+            const startMessage : HTMLParagraphElement = createParagraphWithMessage(message);
+            expect(startMessage.textContent).to.equal(message);
+        })
     });
 });
