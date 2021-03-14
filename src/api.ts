83beb8afb81e24e4bac29e01047ce4bcca49477e
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-03-14 22:21:22 +0000

    create an api class which will hold all the functionality for talking between the server and client! we can now search for item, getAccountById, Register etc

---
+++ src/api.ts
@@ -0,0 +1,80 @@
+
+
+async function get(url: string, params? : Record<string, string>) : Promise<Response> {
+    
+    if(params && typeof params === 'object') {
+        const lastCharacter : string =  String(url.split('').pop());
+        
+        let tempURL : string = url;
+        
+        if(lastCharacter !== '?') {
+            tempURL += '?';
+        }
+
+        tempURL += new URLSearchParams(params);
+
+        return await fetch(tempURL);
+    }
+    return await fetch(url);
+}
+
+async function post(url: string, data: Record<string, string>) : Promise<Response> {
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data)
+    });
+    return response.json();
+}
+
+export class API {
+    private static readonly URL : string = 'https://brimark.api.connieprice.co.uk' 
+    
+    /** GET */
+    public static searchForItem(query?: string) : Promise<Response> {
+        if(!query) {
+            return get(`${API.URL}/Search`);
+        } else {
+            return get(`${API.URL}/Search`, {
+                query: query
+            });
+        }
+    }    
+    
+    public static getAccountById(id: string) : Promise<Response> {
+        return get(`${API.URL}/Account`, {
+            id: id
+        });
+    }
+
+    public static getItemById(id: string) : Promise<Response> {
+        return get(`${API.URL}/Item`, {
+            id: id
+        });
+    }
+
+    /** POST */
+
+    // will need to ask about this one.
+    public static activateAccount(hash: string) : Promise<Response> {
+        return post(`${API.URL}/Activate`, {
+            hash: hash
+        });
+    }
+    
+    public static registerAccount(username: string, email: string, password: string) : Promise<Response> {
+        return post(`${API.URL}/Registration`, {
+            username: username,
+            email: email,
+            password: password
+        });
+    }
+
+    public static getUserInfo(id: string, password: string) : Promise<Response> {
+        return post(`${API.URL}/User`, {
+            id: id,
+            password: password
+        });
+    }
+}
+
+export default API;
