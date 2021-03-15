d224cb680249321c104696d54da40831430d7870
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-03-15 12:02:02 +0000

    updated to include the new and improved namespace, code might look horrible but it's just an example (hasn't been tested yet.)

--- src/login.ts
+++ src/login.ts
@@ -11,7 +11,28 @@ if(loginForm) {
         let usernameOrEmailValue : string = usernameOrEmail.value.trim();
         let passwordValue : string = password.value.trim();
         API.userLogin(usernameOrEmailValue, passwordValue).then(response => {
-            console.log(response);
+            const jsonData = response.json();
+
+            switch(response.status) {
+                case API.StatusCode.OK:
+                    console.log(response);
+                    break;
+                case API.StatusCode.FORBIDDEN:
+                    jsonData.then(data => {
+                        switch(data.error) {
+                            case API.Error.HAS_NOT_ACTIVATED:
+                                // display error to user that they need to activate their account.
+                                break;
+                            case API.Error.INVALID_CREDENTIALS:
+                                // display error about account not found or invalid details (don't want to provide too much information to the user trying to login. Security reasons.)
+                                break;
+                        }
+                    });
+                    break;
+                default:
+                    // weird unknown handled error, display generic error?
+                    break;
+            }
         });
     });
 }
